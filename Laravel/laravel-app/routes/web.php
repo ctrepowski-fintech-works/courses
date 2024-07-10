ea3cf2f2f76b319af68f332ee3131b15996737c6
Caleb Trepowski <ctrepowski@fintech.works>
2024-07-10 17:56:57 -0400

    Refactor job resources files. Add CSRF protection for creating a job resource.

--- Laravel/laravel-app/routes/web.php
+++ Laravel/laravel-app/routes/web.php
@@ -8,9 +8,15 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    return view('jobs', [
-        'jobs' => Job::with('employer')->simplePaginate(10),
+    return view('jobs.index', [
+        'jobs' => Job::with('employer')
+            ->latest()
+            ->simplePaginate(10),
     ]);
+});
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
 });
 
 Route::get('/jobs/{id}', function ($id) {
@@ -20,9 +26,19 @@ Route::get('/jobs/{id}', function ($id) {
         abort(404);
     }
 
-    return view('job', [
+    return view('jobs.show', [
         'job' => $job,
     ]);
+});
+
+Route::post('/jobs', function () {
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1,
+    ]);
+
+    return redirect('/jobs');
 });
 
 Route::get('/contact', function () {
