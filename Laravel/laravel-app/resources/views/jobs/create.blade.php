e666c3c9cacc49a9f3f4da2b8d7fad69236f2fd6
Caleb Trepowski <ctrepowski@fintech.works>
2024-07-10 18:15:24 -0400

    Add server-side validation for forms

--- Laravel/laravel-app/resources/views/jobs/create.blade.php
+++ Laravel/laravel-app/resources/views/jobs/create.blade.php
@@ -17,6 +17,9 @@
                                        class="block flex-1 border-0 py-1.5 px-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                                        placeholder="Software Engineer">
                             </div>
+                            @error('title')
+                                <p class="text-xs text-red-500 font-semibold mt-1">{{ $message }}</p>
+                            @enderror
                         </div>
                     </div>
                     <div class="sm:col-span-4">
@@ -28,6 +31,9 @@
                                        class="block flex-1 border-0 py-1.5 px-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                                        placeholder="$50,000 per Year">
                             </div>
+                            @error('salary')
+                                <p class="text-xs text-red-500 font-semibold mt-1">{{ $message }}</p>
+                            @enderror
                         </div>
                     </div>
                 </div>
