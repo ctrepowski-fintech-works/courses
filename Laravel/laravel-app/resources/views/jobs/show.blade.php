660ffd72e2089a2c320b4acc5ea53e4495f7d45e
Caleb Trepowski <ctrepowski@fintech.works>
2024-07-11 10:33:20 -0400

    Add edit page, add update and destroy request methods handling.

--- Laravel/laravel-app/resources/views/jobs/show.blade.php
+++ Laravel/laravel-app/resources/views/jobs/show.blade.php
@@ -1,5 +1,8 @@
 <x-layout>
     <x-slot:heading>Job Description</x-slot:heading>
-    <h2 class="font-bold text-lg">{{ $job['title'] }}</h2>
-    <p class="mt-6">This job pays {{ $job['salary'] }} per year.</p>
+    <h2 class="font-bold text-lg">{{ $job->title }}</h2>
+    <p class="mt-6">This job pays {{ $job->salary }} per year.</p>
+    <div class="mt-6">
+        <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
+    </div>
 </x-layout>
